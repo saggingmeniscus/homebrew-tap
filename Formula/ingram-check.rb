da27a84a24d6914adf4ddda5a9f158a18407ac19
Jacob Smullyan <smulloni@smullyan.org>
2026-03-11 17:20:47 -0400

    Add ingram-check formula template

---
+++ Formula/ingram-check.rb
@@ -0,0 +1,22 @@
+class IngramCheck < Formula
+  include Language::Python::Virtualenv
+
+  desc "Ingram Lightning Source PDF compliance checker and fixer"
+  homepage "https://github.com/saggingmeniscus/ingram-check"
+  url "https://files.pythonhosted.org/packages/source/i/ingram-checker/ingram_checker-0.1.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  # Resource stanzas will be generated after first PyPI publish:
+  #   brew update-python-resources Formula/ingram-check.rb
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/ingram-check --help")
+  end
+end
